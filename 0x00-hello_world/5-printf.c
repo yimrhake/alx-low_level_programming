0e6e2b6f16232e7ce52c67a29fa85ad7b9dca8a9
sis <sisaygashaw44@gmail.com>
2022-03-28 23:22:54 +0300

    printf function to print a statement with new line

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/*
+ * main function
+ *
+ * Return to 0(Success full)
+ */
+int main(void)
+{
+/* prints a sentence with new line */
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
